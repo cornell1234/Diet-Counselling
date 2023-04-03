d33a15dcfd3c35928dbacce0d926224972ee06f1
Caesar127 <takondwacaesarndawala@gmail.com>
2023-04-03 12:53:15 +0200

    Appointment Model

---
+++ lib/models/appointment_model.dart
@@ -0,0 +1,13 @@
+class Appointment {
+  final String id;
+  final String title;
+  final DateTime dateTime;
+  final String location;
+
+  Appointment({
+    required this.id,
+    required this.title,
+    required this.dateTime,
+    required this.location,
+  });
+}
