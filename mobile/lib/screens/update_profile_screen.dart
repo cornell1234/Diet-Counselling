fd96a1854a20485bbf093e514dbe127f436b06ea
cornellchisao <cornellzchisao@gmail.com>
2023-04-19 15:15:13 +0200

    profilescreen

---
+++ mobile/lib/screens/update_profile_screen.dart
@@ -0,0 +1,12 @@
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(),
+      ),
+    );
+  }
+}
