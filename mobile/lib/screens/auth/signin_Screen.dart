46bb109a5db57d11ae57d70b6967ad209109359b
cornellchisao <cornellzchisao@gmail.com>
2023-04-22 16:23:29 +0200

    signin

--- mobile/lib/screens/auth/signin_Screen.dart
+++ mobile/lib/screens/auth/signin_Screen.dart
@@ -235,7 +235,7 @@ class _signinFormState extends State<signinForm> {
                               color: Colors.grey,
                             ),
                             children: <TextSpan>[
-                              const TextSpan(text: 'Dont have an account yet?'),
+                              const TextSpan(text: 'Dont have an account?'),
                               TextSpan(
                                   text: 'Sign Up',
                                   style: TextStyle(color: Colors.grey[600])),
