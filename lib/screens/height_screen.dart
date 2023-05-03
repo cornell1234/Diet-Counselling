1c9c6b36b287c58c23460cb4d6f79dabb5ad6049
cornellchisao <cornellzchisao@gmail.com>
2023-05-03 16:16:54 +0200

    height

--- lib/screens/height_screen.dart
+++ lib/screens/height_screen.dart
@@ -1,168 +1,64 @@
-// import 'package:flutter/material.dart';
-// import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
 
-// class HeightScreen extends StatefulWidget {
-//   @override
-//   _HeightScreenState createState() => _HeightScreenState();
-// }
+class HeightScreen extends StatefulWidget {
+  @override
+  _HeightScreenState createState() => _HeightScreenState();
+}
 
-// class _HeightScreenState extends State<HeightScreen> {
-//   late CameraController _controller;
-//   late List<CameraDescription> _cameras;
+class _HeightScreenState extends State<HeightScreen> {
+  late CameraController _controller;
+  late List<CameraDescription> _cameras;
 
-//   @override
-//   void initState() {
-//     super.initState();
-//     // Initialize the camera controller
-//     _initializeCamera();
-//   }
+  @override
+  void initState() {
+    super.initState();
+    // Initialize the camera controller
+    _initializeCamera();
+  }
 
-//   void _initializeCamera() async {
-//     // Get available cameras
-//     _cameras = await availableCameras();
+  void _initializeCamera() async {
+    // Get available cameras
+    _cameras = await availableCameras();
 
-//     // Set the camera controller
-//     _controller = CameraController(
-//       // Use the first available camera
-//       _cameras[0],
-//       ResolutionPreset.high,
-//     );
+    // Set the camera controller
+    _controller = CameraController(
+      // Use the first available camera
+      _cameras[0],
+      ResolutionPreset.high,
+    );
 
-//   class _HeightScreenState extends State<HeightScreen> {
-//   CameraController _controller;
-//   List<CameraDescription> _cameras;
+    // Initialize the controller
+    await _controller.initialize();
 
-//   double _height;
+    // Start the camera preview
+    setState(() {});
+  }
 
-//   @override
-//   void initState() {
-//     super.initState();
-//     // Initialize the camera controller
-//     _initializeCamera();
-//   }
+  @override
+  void dispose() {
+    // Dispose the camera controller
+    _controller.dispose();
+    super.dispose();
+  }
 
-//   void _initializeCamera() async {
-//     // Get available cameras
-//     _cameras = await availableCameras();
+  @override
+  Widget build(BuildContext context) {
+    // If the controller is null, display a loading indicator
+    if (_controller == null || !_controller.value.isInitialized) {
+      return Center(child: CircularProgressIndicator());
+    }
 
-//     // Set the camera controller
-//     _controller = CameraController(
-//       // Use the first available camera
-//       _cameras[0],
-//       ResolutionPreset.high,
-//     );
+    // Otherwise, display the camera preview
+    return AspectRatio(
+      aspectRatio: _controller.value.aspectRatio,
+      child: CameraPreview(_controller),
+    );
+  }
+}
 
-//     // Initialize the controller
-//     await _controller.initialize();
-
-//     // Start the camera preview
-//     setState(() {});
-//   }
-
-//   @override
-//   void dispose() {
-//     // Dispose the camera controller
-//     _controller.dispose();
-//     super.dispose();
-//   }
-
-//   Future<void> _takePicture() async {
-//     // Check if the controller is initialized
-//     if (!_controller.value.isInitialized) {
-//       return;
-//     }
-
-//     try {
-//       // Take a picture
-//       final image = await _controller.takePicture();
-
-//       // Process the image to extract the height information
-//       // ...
-
-//       // Update the height variable
-//       setState(() {
-//         _height = height; // replace with the extracted height
-//       });
-//     } catch (e) {
-//       print(e);
-//     }
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // If the controller is null, display a loading indicator
-//     if (_controller == null || !_controller.value.isInitialized) {
-//       return Center(child: CircularProgressIndicator());
-//     }
-
-//     // Otherwise, display the camera preview and the height measurement
-//     return Stack(
-//       children: [
-//         AspectRatio(
-//           aspectRatio: _controller.value.aspectRatio,
-//           child: CameraPreview(_controller),
-//         ),
-//         Align(
-//           alignment: Alignment.bottomCenter,
-//           child: Container(
-//             color: Colors.black,
-//             child: Row(
-//               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//               children: [
-//                 IconButton(
-//                   icon: Icon(Icons.camera_alt),
-//                   onPressed: _takePicture,
-//                   color: Colors.white,
-//                 ),
-//                 Text(
-//                   _height != null ? 'Height: $_height cm' : '',
-//                   style: TextStyle(
-//                     color: Colors.white,
-//                     fontSize: 20,
-//                   ),
-//                 ),
-//               ],
-//             ),
-//           ),
-//         ),
-//       ],
-//     );
-//   }
-
-//     // Initialize the controller
-//     await _controller.initialize();
-
-//     // Start the camera preview
-//     setState(() {});
-//   };
-
-//   @override
-//   void dispose() {
-//     // Dispose the camera controller
-//     _controller.dispose();
-//     super.dispose();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // If the controller is null, display a loading indicator
-//     if (_controller == null || !_controller.value.isInitialized) {
-//       return Center(child: CircularProgressIndicator());
-//     }
-
-//     // Otherwise, display the camera preview
-//     return AspectRatio(
-//       aspectRatio: _controller.value.aspectRatio,
-//       child: CameraPreview(_controller),
-//     );
-    
-//   }
-
-// }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // TODO: implement build
-//     throw UnimplementedError();
-//   }
-// }
+@override
+Widget build(BuildContext context) {
+  // TODO: implement build
+  throw UnimplementedError();
+}
